09be38af89e6640205bcace0b028c4301e0d569a
Noma <wiostz98kr@gmail.com>
2021-10-11 17:45:58 +0900

    Make counter use bindActionCreators( )

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,27 +1,33 @@
 import React from 'react'
 import Counter from '../components/Counter'
 import { increase, decrease, setDiff } from '../modules/counter'
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
-function CounterContainer({ diff, number, onIncrease, onDecrease, onSetDiff }) {
+function CounterContainer({ diff, number, increase, decrease, setDiff }) {
     return <Counter
         //상태 넣어주기
         diff={diff}
         number={number}
         //액션을 디스패치해 주는 함수 넣어주기
-        onIncrease={onIncrease}
-        onDecrease={onDecrease}
-        onSetDiff={onSetDiff}
+        onIncrease={increase}
+        onDecrease={decrease}
+        onSetDiff={setDiff}
     />
 }
 const mapStateToProps = state => ({
     number: state.counter.number,
     diff: state.counter.diff
 });
-const mapDispatchToProps = dispatch => ({
-    onIncrease: () => dispatch(increase()),
-    onDecrease: () => dispatch(decrease()),
-    onSetDiff: (diff) => dispatch(setDiff(diff))
-});
+const mapDispatchToProps = dispatch =>
+    bindActionCreators(
+        {
+            increase,
+            decrease,
+            setDiff
+        },
+        dispatch
+    );
+
 
 export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
