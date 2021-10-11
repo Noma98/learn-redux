e907b7d03ba6799c3903dc5236308962e3fcc555
Noma <wiostz98kr@gmail.com>
2021-10-11 17:41:56 +0900

    Make counter use connect( )

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,18 +1,9 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
-import { useSelector } from 'react-redux'
 import Counter from '../components/Counter'
 import { increase, decrease, setDiff } from '../modules/counter'
+import { connect } from 'react-redux';
 
-function CounterContainer() {
-    // useSelector:리덕스 스토어의 상태를 조회하는 훅
-    const { number, diff } = useSelector(state => state.counter)
-    // useDispatch:리덕스 스토어의 dispatch를 사용할 수 있도록 하는 훅
-    const dispatch = useDispatch();
-
-    const onIncrease = () => dispatch(increase());
-    const onDecrease = () => dispatch(decrease());
-    const onSetDiff = diff => dispatch(setDiff(diff));
+function CounterContainer({ diff, number, onIncrease, onDecrease, onSetDiff }) {
     return <Counter
         //상태 넣어주기
         diff={diff}
@@ -23,5 +14,14 @@ function CounterContainer() {
         onSetDiff={onSetDiff}
     />
 }
+const mapStateToProps = state => ({
+    number: state.counter.number,
+    diff: state.counter.diff
+});
+const mapDispatchToProps = dispatch => ({
+    onIncrease: () => dispatch(increase()),
+    onDecrease: () => dispatch(decrease()),
+    onSetDiff: (diff) => dispatch(setDiff(diff))
+});
 
-export default CounterContainer
+export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
